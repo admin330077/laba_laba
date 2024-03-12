5bf28d52cd998af50a2948e9700431089e358e9f
MrZagreed2006 <163097081+MrZagreed2006@users.noreply.github.com>
2024-03-12 09:34:31 +0300

    ремейк
    внесение изменений

--- index.py
+++ index.py
@@ -1,13 +1,13 @@
 import math
 
-def count_points_in_circle(radius):
+def count_points_in_circle(radius):#вычисляет количество точек окружности
     count = 0
     for x in range(-radius, radius + 1):
         for y in range(-radius, radius + 1):
             if x**2 + y**2 <= radius**2:
                 count += 1
     return count
-
+#пример использования
 radius = int(input('ввидите число'))
 result = count_points_in_circle(radius)
-print(f"Всего  точек в радиусе {radius}")
+print(f"Всего  точек в радиусе {radius}")  
